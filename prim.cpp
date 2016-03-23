f594e0778ef733eadc4186d0ffcf6648ff9cc323
niyaowoa <407354967@qq.com>
2016-03-23 19:45:17 +0800

    最小生成树---prim算法
    1).输入：一个加权连通图，其中顶点集合为V，边集合为E；
    
    2).初始化：Vnew = {x}，其中x为集合V中的任一节点（起始点），Enew = {},为空；
    
    3).重复下列操作，直到Vnew = V：
    
    a.在集合E中选取权值最小的边<u, v>，其中u为集合Vnew中的元素，而v不在Vnew集合当中，并且v∈V（如果存在有多条满足前述条件即具有相同权值的边，则可任意选取其中之一）；
    
    b.将v加入集合Vnew中，将<u, v>边加入集合Enew中；
    
    4).输出：使用集合Vnew和Enew来描述所得到的最小生成树。

--- prim.cpp
+++ prim.cpp
@@ -1,3 +1,17 @@
+/*
+1).输入：一个加权连通图，其中顶点集合为V，边集合为E；
+
+2).初始化：Vnew = {x}，其中x为集合V中的任一节点（起始点），Enew = {},为空；
+
+3).重复下列操作，直到Vnew = V：
+
+a.在集合E中选取权值最小的边<u, v>，其中u为集合Vnew中的元素，而v不在Vnew集合当中，并且v∈V（如果存在有多条满足前述条件即具有相同权值的边，则可任意选取其中之一）；
+
+b.将v加入集合Vnew中，将<u, v>边加入集合Enew中；
+
+4).输出：使用集合Vnew和Enew来描述所得到的最小生成树。
+
+*/
 #include <cstdio>
 #include <cmath>
 #include <cstdlib>
@@ -24,7 +38,7 @@ int c[maxn][maxn]={//0 1 2 3 4 5
       (4)-------(5)
             9          
  */
-
+ 
 int s[maxn];//判断顶点是否在s里面
 int lowest[maxn];//每次到s的最小距离
 void prim(int n){
